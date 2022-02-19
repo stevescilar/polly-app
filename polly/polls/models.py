3657e6ee31d9d2c8cfe565234120e91b6e7f42af
stevescilar <stevemuambi@gmail.com>
2022-02-19 22:55:48 +0300

    some models on pollu

--- polly/polls/models.py
+++ polly/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Question(models.model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('Date Published')
 # Create your models here.
+ 
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
