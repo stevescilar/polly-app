483c8de24d7d8661dd2f0748754431bdf3937a4c
stevescilar <stevemuambi@gmail.com>
2022-02-19 23:09:47 +0300

    more edits

--- polly/polls/models.py
+++ polly/polls/models.py
@@ -1,4 +1,8 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -6,6 +10,8 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 # Create your models here.
  
 class Choice(models.Model):
